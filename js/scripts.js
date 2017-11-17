6a8bb4914dbf0ac696b17022ce4fd91ab7901baa
ESTEI9 <eric.steinecke@gmail.com>
2017-11-17 11:24:02 -0500

    Update to 1.25
    Added a genuine review. Changed formatting of reviews section per new
    data (i.e. no pic or stars and longer reviews).

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,19 @@
-//V 1.21
+//V 1.25
 
 'use strict';
 
 // Init Variables
 let reviews = [
     {
-        author:'Eric Steinecke',
-        picUrl: '',
-        rating: 'three',
-        quote: 'Lorm ipsum dolor sit amet...'
+        author:'Richard H. Halderman',
+        title: 'Retired College Public Relations Director',
+        quote: '<i>Death of a Teacher</i> is a love story on many levels...love of a profession, teaching, and especially those men and women who face their own personal demons while battling administrative bureaucracy, overcrowded classrooms, and lack of supplies in attempting to teach dozens of children. It is love of these children, too, who bring with them the problems of home and society. It is a story of a romantic, forbidden love, of love of family, and of a young man’s quest to find and love himself.  With sympathy and understanding, with humor and gentle satire, McShain tells this story through vibrant characters facing personal and physical handicaps.  That many find redemption through love is what sets this novel apart from earlier novels with schoolroom settings.'
     },
     {
-        author:'',
-        picUrl: '',
-        rating: 'five',
-        quote: 'Lorm ipsum dolor sit amet...'
-    },
-    {
-        author:'',
-        picUrl: '',
-        rating: 'four',
-        quote: 'Lorm ipsum dolor sit amet...'
-    },
-    {
-        author:'',
-        picUrl: '',
-        rating: 'three',
-        quote: 'Lorm ipsum dolor sit amet...'
-    },
-    {
-        author:'',
-        picUrl: '',
-        rating: 'five',
-        quote: 'Lorm ipsum dolor sit amet...'
-    },
-    {
-        author:'',
-        picUrl: '',
-        rating: 'five',
-        quote: 'Lorm ipsum dolor sit amet...'
-    },
+        author: 'Eric Steinecke',
+        title: 'Front End Web Developer',
+        quote: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam maximus dictum dui, et sodales nibh. Etiam fringilla, tellus elementum semper finibus, nunc odio feugiat dolor, at ornare velit sem vitae ipsum. Proin sed lorem ultricies, aliquet ante quis, mattis leo. Curabitur lobortis nunc at ligula viverra scelerisque. Pellentesque vel odio fermentum, suscipit justo vitae, finibus ante. Mauris eu ullamcorper lectus, sit amet lobortis lacus. Pellentesque fermentum mattis auctor. Proin fermentum magna eget arcu viverra volutpat. Aliquam gravida quam aliquet dui aliquet viverra. Mauris quis dolor tempus, dictum lacus venenatis, vehicula urna. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed blandit libero eget nibh volutpat tincidunt. Vivamus dapibus facilisis eros a volutpat. Nunc posuere commodo cursus. Nulla non dictum urna. Donec elementum, nibh ac rutrum mattis, lectus nisl euismod mi, a vestibulum ipsum elit at nibh.'
+    }
 ];
 
 jQuery(document).ready(function($){
@@ -60,53 +34,38 @@ jQuery(document).ready(function($){
     });
     
     for (let i=0; i<reviews.length; i++) {
-        let picUrl = (reviews[i].picUrl === '') ? 'assets/guyfawkes.png' : reviews[i].picUrl;
-        let author = (reviews[i].author === '') ? 'Anonymous' : reviews[i].author;
+        let title = (reviews[i].title === null) ? 'Book Reviewer' : reviews[i].title;
         
         $('#reviews-slider').append(
-        "<div class='slide'>\
-        <div class='review-img' style='background-image:url("+picUrl+");'></div>\
-        <h3>"+author+"</h3>\
-        <div class='rating "+reviews[i].rating+"-star'>\
-            <span></span><span></span><span></span><span></span><span></span>\
-        </div>\
-        <q>"+reviews[i].quote+"</q>\
-        </div>"
+        `<div class='slide'>
+            <div class='container'>
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="review">
+                            <h3>`+reviews[i].author+`</h3>
+                            <span class="author-title">`+title+`</span>
+                            <q>`+reviews[i].quote+`</q>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
         );
     }
     
     $('#reviews-slider').slick({
-        slidesToShow:4,
-        slidesToScroll:3,
+        slidesToShow:1,
+        slidesToScroll:1,
         infinite:true,
         prevArrow: $('#prevArrow'),
         nextArrow: $('#nextArrow'),
+        swipe:false,
         responsive: [
-            {
-                breakpoint:1280,
-                settings: {
-                    arrows:false
-                }
-            },
             {
                 breakpoint:992,
                 settings: {
-                    slidesToShow:3,
-                    slidesToScroll:2
-                }
-            },
-            {
-                breakpoint:768,
-                settings: {
-                    slidesToShow:2,
-                    slidesToScroll:2
-                }
-            },
-            {
-                breakpoint:481,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll:1
+                    arrows:false,
+                    swipe: true
                 }
             }
         ]
